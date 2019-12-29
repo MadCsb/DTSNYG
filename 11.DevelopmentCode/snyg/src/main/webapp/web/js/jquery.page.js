975dcfb7be1e3445501f9b5a66f4db4de7d33f33
chengsbDesktopComputer <chengsb_register@163.com>
2019-12-29 12:45:49 +0800

    bug

--- 11.DevelopmentCode/snyg/src/main/webapp/web/js/jquery.page.js
+++ 11.DevelopmentCode/snyg/src/main/webapp/web/js/jquery.page.js
@@ -11,12 +11,13 @@
         obj.empty();
         //上一页
         if(args.current > 1){
-          obj.append('<a prevPage="prevPage"><  上一页</a>');
+          obj.append('<li><span prevPage="prevPage">上一页</span></li>');
         }else{
-          obj.append('<a prevPage=""><  上一页</a>');
+          obj.append('<li><span prevPage="">上一页</span></li>');
         }
+
         if(args.current != 1 && args.current >= 4 && args.pageCount != 4){ 
-          obj.append('<a page="1">'+1+'</a>');
+          obj.append('<li><span page="1">'+1+'</span></li>');
         } 
         if(args.current-2 > 2 && args.current <= args.pageCount && args.pageCount > 5){ 
           obj.append('...');
@@ -31,9 +32,9 @@
         for (;start <= end; start++) { 
           if(start <= args.pageCount && start >= 1){ 
             if(start != args.current){ 
-              obj.append('<a page="'+ start +'">'+ start +'</a>');
+              obj.append('<li><span page="'+ start +'">'+ start +'</span></li>');
             }else{ 
-              obj.append('<a class="current">'+ start +'</a>');
+              obj.append('<li><span class="currentpage">'+ start +'</span></li>');
             }
           }
         } 
@@ -41,46 +42,47 @@
           obj.append('...');
         } 
         if(args.current != args.pageCount && args.current < args.pageCount -2 && args.pageCount != 4){ 
-          obj.append('<a page="'+ args.pageCount +'">'+args.pageCount+'</a>');
+          obj.append('<li><span page="'+ args.pageCount +'">'+args.pageCount+'</span></li>');
         }
         if(args.current < args.pageCount){ 
-          obj.append('<a nextPage="nextPage">下一页  ></a>');
+          obj.append('<li><span nextPage="nextPage">下一页</span></li>');
         }else{
-          obj.append('<a nextPage="">下一页  ></a>');
+          obj.append('<li><span nextPage="">下一页</span></li>');
         } 
       })(); 
     },
     bindEvent:function(obj,args){ 
-      return (function(){ 
-        obj.on("click","a[page]",function(){
+      return (function(){
+        obj.find("span[page]").click(function(){
           var current = parseInt($(this).attr("page"));
-          ms.fillHtml(obj,{"current":current,"pageCount":args.pageCount}); 
-          if(typeof(args.backFn)=="function"){ 
-            args.backFn(current); 
-          } 
+          ms.fillHtml(obj,{"current":current,"pageCount":args.pageCount});
+          if(typeof(args.backFn)=="function"){
+            args.backFn(current);
+          }
         });
         //上一页
-        obj.on("click","a[prevPage]",function(){
+        obj.find("span[prevPage]").click(function(){
           if($(this).attr('prevPage') == 'prevPage')
           {
-            var current = parseInt(obj.children("a.current").text());
+            var current = parseInt(obj.find("span.currentpage").text());
             ms.fillHtml(obj,{"current":current-1,"pageCount":args.pageCount});
             if(typeof(args.backFn)=="function"){
               args.backFn(current-1);
             }
           }
         });
+
         //下一页
-        obj.on("click","a[nextPage]",function(){
+        obj.find("span[nextPage]").click(function(){
           if($(this).attr('nextPage') == 'nextPage')
           {
-            var current = parseInt(obj.children("a.current").text());
+            var current = parseInt(obj.find("span.currentpage").text());
             ms.fillHtml(obj,{"current":current+1,"pageCount":args.pageCount});
             if(typeof(args.backFn)=="function"){
               args.backFn(current+1);
             }
           }
-        }); 
+        });
       })(); 
     } 
   } 
